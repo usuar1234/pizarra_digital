9e9b7dbee2ce6b927b973d48785dc347409d16e0
jguerralla <contactojoseantonioguerra@gmail.com>
2018-07-14 13:46:09 +0200

    nuevo diseno, mejoras generales

--- pizarra_digital.js
+++ pizarra_digital.js
@@ -1,34 +1,31 @@
-var canvas = document.getElementById("areaDibujo")
-var papel = canvas.getContext("2d")
+var canvas = document.getElementById('areaDibujo');
+var papel = canvas.getContext('2d');
 
 var teclas = {UP: 38, DOWN: 40, LEFT: 37, RIGHT: 39};
 document.addEventListener('keydown', dibujarTeclado);
 
 function dibujarTeclado(evento)
 {
-    var movimiento = 5;
-    switch (evento.keyCode)
-    {
-      case teclas.UP:
-        dibujarLinea(color, x, y, x, y - movimiento);
-        y = y - movimiento;
-      break;
-      case teclas.DOWN:
-        dibujarLinea(color, x, y, x, y + movimiento);
-        y = y + movimiento;
-      break;
-      case teclas.LEFT:
-        dibujarLinea(color, x, y, x - movimiento, y);
-        x = x - movimiento;
-      break;
-      case teclas.RIGHT:
-        dibujarLinea(color, x, y, x + movimiento, y);
-        x = x + movimiento;
-      break;
-      default:
-        console.log("Otra tecla");
-        break;
-    }
+  var movimiento = 5;
+  switch (evento.keyCode)
+  {
+    case teclas.UP:
+      dibujarLinea(color, x, y, x, y - movimiento);
+      y = y - movimiento;
+    break;
+    case teclas.DOWN:
+      dibujarLinea(color, x, y, x, y + movimiento);
+      y = y + movimiento;
+    break;
+    case teclas.LEFT:
+      dibujarLinea(color, x, y, x - movimiento, y);
+      x = x - movimiento;
+    break;
+    case teclas.RIGHT:
+      dibujarLinea(color, x, y, x + movimiento, y);
+      x = x + movimiento;
+    break;
+  }
 }
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
@@ -72,13 +69,23 @@ function dibujarMouse(evento)
 }
 
 var color_lapiz = document.getElementById('color_lapiz');
-color_lapiz.addEventListener("change", colorLapiz);
+color_lapiz.addEventListener('change', colorLapiz);
 
 var color = 'black';
 
 function colorLapiz()
 {
   color = color_lapiz.value;
+}
+
+var color_fondo = document.getElementById('color_fondo');
+color_fondo.addEventListener('change', colorFondo);
+
+function colorFondo()
+{
+  fondo = color_fondo.value;
+  var areaDibujo = document.getElementById("areaDibujo");
+  areaDibujo.style.backgroundColor = fondo;
 }
 
 var tamaño_lapiz = document.getElementById('tamaño_lapiz');
@@ -96,5 +103,10 @@ limpiar_lienzo.addEventListener('click', limpiarLienzo)
 
 function limpiarLienzo(evento)
 {
-  papel.clearRect(0, 0, canvas.width, canvas.height);
+  var bool = confirm('Seguro que desea limpiar todo?');
+  if (bool)
+  {
+    papel.clearRect(0, 0, canvas.width, canvas.height);
+    alert('Se eliminó correctamente');
+  }
 }
